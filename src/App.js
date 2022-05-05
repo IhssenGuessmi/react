5cca94be15a7b7eed70a36b64205a546ca5130df
Ihssen Guessmi <ihssenguessmi@gmail.com>
2022-05-05 17:35:19 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button , Form , Navbar , Container , Nav} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+      <Form>
+  <fieldset disabled>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+      <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+      <Form.Select id="disabledSelect">
+        <option>Disabled select</option>
+      </Form.Select>
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Check
+        type="checkbox"
+        id="disabledFieldsetCheck"
+        label="Can't check this"
+      />
+    </Form.Group>
+    <Button type="submit">Submit</Button>
+  </fieldset>
+</Form>
+
+<Button variant="danger">Start</Button>
     </div>
   );
 }
